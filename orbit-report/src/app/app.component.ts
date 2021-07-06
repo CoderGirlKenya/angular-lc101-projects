78efcfaeafd2fb9687808d932c4421211c9d502e
CoderGirlKenya <kenya78hami@gmail.com>
2021-07-05 20:32:14 -0500

    completed part 7 and started on 8

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { JsonPipe } from '@angular/common';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
 
@@ -10,13 +11,13 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite [];
-  displayList: Satellite [];
+  displayList:  Satellite [];
   constructor() {
-      this.displayList = [];
+    this.displayList = [];
       this.sourceList = [];
       let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
    
-   
+    
       window.fetch(satellitesUrl).then(function(response) {
          response.json().then(function(data){
           let fetchedSatellites = data.satellites;
@@ -24,29 +25,34 @@ export class AppComponent {
           for(let i=0; i < fetchedSatellites.length;i++){
               let newSatellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType,fetchedSatellites[i].operational)
               this.sourceList.push(newSatellite)
+              this.displayList = this.sourceList.slice(0);
+             // this.displayList = matchingSatellites;
             };
                
          }.bind(this));
         }.bind(this));
-         // make a copy of the sourceList to be shown to the user
-      
-};  
-    search(searchTerm: string): void {
-      let matchingSatellites: Satellite[] = [];
-      searchTerm = searchTerm.toLowerCase();
-      for(let i=0; i < this.sourceList.length; i++) {
-         let name = this.sourceList[i].name.toLowerCase();
-         if (name.indexOf(searchTerm) >= 0) {
-            matchingSatellites.push(this.sourceList[i]);
-         }
+            
+        
+          
+  };
+  search(searchTerm: string): void {
+   let matchingSatellites: Satellite[] = [];
+   searchTerm = searchTerm.toLowerCase();
+   for(let i=0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+         matchingSatellites.push(this.sourceList[i]);
       }
-      // assign this.displayList to be the array of matching satellites
-      // this will cause Angular to re-make the table, but now only containing matches
-      this.displayList = matchingSatellites;
    }
+   // assign this.displayList to be the array of matching satellites
+   // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
+     
+     
   };
       
-   
+
    
  
 
